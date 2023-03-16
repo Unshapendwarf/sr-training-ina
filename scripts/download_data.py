6a37d0c45abd12bfc4f09d8191736dc747ee25ac
Uitaek Hong <dmlxor8422@gmail.com>
2023-03-16 08:57:56 -0700

    small modification

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,14 +1,16 @@
 import os, zipfile, gdown
 
 url = 'https://drive.google.com/uc?id=1ussHhGVh0BEe_RjyGgD3lS3rJNwtOc4R'
-out_path = os.path.join("../data.zip")
+
+# make sure that you are in the root of working directory
+out_path = os.path.join("./data.zip")
 if not os.path.exists(out_path):
     gdown.download(url, out_path, quiet=False)
 else:
     print(f"already exists: {out_path}")
 
 data_zip = zipfile.ZipFile(out_path)
-unzip_path = os.path.join("../data")
+unzip_path = os.path.join("./data")
 
 if not os.path.exists(unzip_path):
     data_zip.extractall(unzip_path)
