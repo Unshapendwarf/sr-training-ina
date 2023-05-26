0aafecc5d731655fb7509befe19015a41f155809
Yunheon Lee <yhlee@mango3.kaist.ac.kr>
2023-05-26 15:09:30 +0900

    Add pretrained model option -m in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,13 +6,22 @@ n_cls=20
 
 SCRIPTDIR=$(dirname $0)
 BASEDIR=$(realpath "${SCRIPTDIR}/../")
+MODEL_DIR='logs'
+
+while getopts m: opt
+do
+      case $opt in
+            m) MODEL_DIR=$OPTARG
+            ;;
+      esac
+done
 
 python test.py --data_root  ${BASEDIR}/data \
       --model_type EDSR \
       --scale 4 --n_blocks $n_b --n_feats $n_f \
       --use_cuda --num_epoch 10 --num_valid_image 3\
       --num_batch 1 \
-      --pretrained --pretrained_dir logs\
+      --pretrained --pretrained_dir $MODEL_DIR\
       --cluster --cluster_num 20 --img_save
 
 
